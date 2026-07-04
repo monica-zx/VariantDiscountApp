8ae06eabe77e6b44ea7aa4b7bf02387bbcf39886
Monica Ranshur <monica.ranshur@zedexinfo.com>
2026-07-04 19:07:18 +0530

    Fix Vercel preset configuration for React Router

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { vercelPreset } from "@vercel/react-router/vite";
+// import { vercelPreset } from "@vercel/react-router/vite";
 
 // Related: https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
 // Replace the HOST env var with SHOPIFY_APP_URL so that it doesn't break the Vite server.
@@ -50,9 +50,7 @@ export default defineConfig({
     },
   },
  plugins: [
-    reactRouter({
-      presets: [vercelPreset()],
-    }),
+    reactRouter(),
     tsconfigPaths(),
   ],
   build: {
